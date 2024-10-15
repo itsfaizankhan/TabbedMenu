2ecca2dfdbf1213a4b7889fc85709dbe4c2fa4a8
Faizan Khan <71210312+itsfaizankhan@users.noreply.github.com>
2024-10-15 23:26:19 +0530

    Dynamically loading widgets instead of loading everything and only showing active widgets. Works but code needs to be cleaned.

--- utility_horizontal.py
+++ utility_horizontal.py
@@ -1,6 +1,6 @@
 from textual.app import App, ComposeResult
 from textual.containers import Horizontal, Vertical
-from textual.widgets import OptionList, RichLog
+from textual.widgets import OptionList, RichLog, Static
 from textual.widgets.option_list import Option
 
 
@@ -23,22 +23,61 @@ class Sidebar(Vertical):
         )
 
 
-class MenuContainer(Vertical):
+class SubMenu(Static):
+    def __init__(self, menu_id: str, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.menu_id = menu_id
+        self._contents = [
+            Option(f"Menu {self._id} Option 1"),
+            Option(f"Menu {self._id} Option 2"),
+            Option(f"Menu {self._id} Option 3"),
+            Option(f"Menu {self._id} Option 4"),
+            Option(f"Menu {self._id} Option 5"),
+            Option(f"Menu {self._id} Option 6"),
+            Option(f"Menu {self._id} Option 7"),
+            Option(f"Menu {self._id} Option 8"),
+            Option(f"Menu {self._id} Option 9"),
+        ]
+
+        self.render()
+        # if self.menu_id:
+        #     self.add_options(
+        #         [
+        #             Option(f"Menu {self.menu_id} Option 1"),
+        #             Option(f"Menu {self.menu_id} Option 2"),
+        #             Option(f"Menu {self.menu_id} Option 3"),
+        #             Option(f"Menu {self.menu_id} Option 4"),
+        #             Option(f"Menu {self.menu_id} Option 5"),
+        #             Option(f"Menu {self.menu_id} Option 6"),
+        #             Option(f"Menu {self.menu_id} Option 7"),
+        #             Option(f"Menu {self.menu_id} Option 8"),
+        #             Option(f"Menu {self.menu_id} Option 9"),
+        #         ]
+        #     )
+
     def compose(self) -> ComposeResult:
-        for i in range(10):
-            yield OptionList(
-                Option(f"Menu {i+1} Option 1"),
-                Option(f"Menu {i+1} Option 2"),
-                Option(f"Menu {i+1} Option 3"),
-                Option(f"Menu {i+1} Option 4"),
-                Option(f"Menu {i+1} Option 5"),
-                Option(f"Menu {i+1} Option 6"),
-                Option(f"Menu {i+1} Option 7"),
-                Option(f"Menu {i+1} Option 8"),
-                Option(f"Menu {i+1} Option 9"),
-                classes="box hidden",
-                id=f"o{i+1}-menu",
-            )
+        yield OptionList(
+            Option(f"Menu {self.menu_id} Option 1"),
+            Option(f"Menu {self.menu_id} Option 2"),
+            Option(f"Menu {self.menu_id} Option 3"),
+            Option(f"Menu {self.menu_id} Option 4"),
+            Option(f"Menu {self.menu_id} Option 5"),
+            Option(f"Menu {self.menu_id} Option 6"),
+            Option(f"Menu {self.menu_id} Option 7"),
+            Option(f"Menu {self.menu_id} Option 8"),
+            Option(f"Menu {self.menu_id} Option 9"),
+            classes="box",
+            id=f"o{self.menu_id}-menu",
+        )
+
+
+# class SubMenuContainer(Vertical):
+#     def compose(self) -> ComposeResult:
+#         # yield ScrollableContainer(Static(), id="dynamic-submenus")
+#         yield SubMenu(id="submenu")
+#
+#     def mount_submenu(self):
+#         pass
 
 
 class UtilityContainers(App):
@@ -47,29 +86,64 @@ class UtilityContainers(App):
     def compose(self) -> ComposeResult:
         with Horizontal():
             yield Sidebar(classes="column")
-            yield MenuContainer(classes="column")
+            # yield SubMenuContainer(classes="column")
+            yield Vertical(id="submenu-container", classes="column")
             yield RichLog(highlight=True, markup=True)
 
     def on_option_list_option_selected(self, event: OptionList.OptionSelected) -> None:
         """Event handler called when an option in OptionList is selected."""
         option_id = event.option.id
         app_log = self.query_one(RichLog)
+        prev_submenu = None
+        # curr_submenu = None
 
-        if not option_id or len(option_id) >= 4:
+        if not option_id or len(option_id) >= 2:
+            if option_id:
+                app_log.write("option_id: " + option_id)
             return
 
         app_log.write(option_id)
-        menus = self.query(OptionList).exclude("#sidebar")
+        submenu_container = self.query("#submenu-container").last()
 
-        menu_to_hide = menus.exclude(".hidden")
-        for menu in menu_to_hide:
-            menu.add_class("hidden")
+        if submenu_container:
+            app_log.write("Before: ")
+            app_log.write(submenu_container.children)
+            if len(submenu_container.children) != 0:
+                app_log.write("First child before clearing node:")
+                prev_submenu = submenu_container.children[0]
+                # app_log.write(prev_submenu._id)
+                submenu_container.remove_children("*")
 
-        menu_to_show = menus.filter(f"#o{option_id}-menu").first()
-        menu_to_show.remove_class("hidden")
-        menu_to_show.focus()
+        submenu_container = self.query("#submenu-container").last()
+        # new_submenu = SubMenu(menu_id=f"o{option_id}")
+        submenu_container.mount(SubMenu(menu_id=f"o{option_id}", id=f"o{option_id}"))
 
-        app_log.write(menu_to_show)
+        app_log.write("After: ")
+        app_log.write(submenu_container.children)
+
+        app_log.write("-----")
+        if submenu_container:
+            app_log.write("Post children: ")
+            app_log.write(submenu_container.children)
+            app_log.write("First child: ")
+            if len(submenu_container.children) != 0:
+                prev_submenu = submenu_container.children[0]
+                app_log.write(prev_submenu)
+
+        # if submenu_container.first().id != option_id:
+        #     pass
+
+        # menus = self.query(OptionList).exclude("#sidebar")
+        #
+        # menu_to_hide = menus.exclude(".hidden")
+        # for menu in menu_to_hide:
+        #     menu.add_class("hidden")
+        #
+        # menu_to_show = menus.filter(f"#o{option_id}-menu").first()
+        # menu_to_show.remove_class("hidden")
+        # menu_to_show.focus()
+        #
+        # app_log.write(menu_to_show)
 
 
 if __name__ == "__main__":
